51265902f180f3f643c164608e7d27b20271e96b
Petro Fediushko <fedyushko2201@gmail.com>
2018-06-08 00:01:45 +0300

    Clear Code

--- Pacman/Position.cs
+++ Pacman/Position.cs
@@ -5,11 +5,6 @@
         public int X { get;set; }
         public int Y { get;set; }
 
-        public int XminusOne => X--;
-        public int XplusOne => X++;
-        public int YminusOne => Y--;
-        public int YplusOne =>Y++;
-
         public Position(int x, int y)
         {
             X = x;
