7f04d0c03a51d0ae633db7c74ac91dffa9d865e9
Petro Fediushko <fedyushko2201@gmail.com>
2018-07-07 09:38:09 +0300

    Create web project

---
+++ PacmanWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PacmanWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
